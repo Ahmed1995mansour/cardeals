387df903df7621413895595ddd9add9f51531499
ahmed1995mansour <Ahmed1995mansour@gmail.com>
2024-02-12 22:52:59 +0200

    add firebase

---
+++ client/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: 'cardeals-2024.firebaseapp.com',
+  projectId: 'cardeals-2024',
+  storageBucket: 'cardeals-2024.appspot.com',
+  messagingSenderId: '694725881041',
+  appId: '1:694725881041:web:8f490f7e1d321c5cfee9f0',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
